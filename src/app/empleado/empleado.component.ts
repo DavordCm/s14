7424b81ed9bbd83f3c801912ba662e9730f4d872
DavordCm <jhoosman.xd@gmail.com>
2024-08-12 15:23:40 -0500

    mejorando la pagina v0.1

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
+
+import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../angular-material/material/material.module';
+
 
 @Component({
   selector: 'app-empleado',
   standalone: true,
-  imports: [MatButtonModule,MatCardModule, MatChipsModule, MatProgressBarModule],
+  imports: [MaterialModule,FormsModule,MatCardModule, MatChipsModule, MatProgressBarModule],
   templateUrl: './empleado.component.html',
   styleUrl: './empleado.component.css'
 })
@@ -15,4 +18,9 @@ export class EmpleadoComponent {
   longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
   Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
   usually kept as a companion animal or for showing.`;
+
 }
+function signal<T>(arg0: { name: string; completed: boolean; subtasks: { name: string; completed: boolean; }[]; }) {
+  throw new Error('Function not implemented.');
+}
+
