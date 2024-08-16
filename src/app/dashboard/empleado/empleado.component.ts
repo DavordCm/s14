472fbcbca10ce85c5cf0a156c0a59c9bfcf5795b
DavordCm <jhoosman.xd@gmail.com>
2024-08-16 04:42:28 -0500

    Día 5 un pequeño diseño sobre multas

--- src/app/dashboard/empleado/empleado.component.ts
+++ src/app/dashboard/empleado/empleado.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-empleado',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatTableModule ],
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent {
 empleados: any;
+displayedColumns: string[] = ['$implicit', 'index', 'count', 'first', 'last', 'even', 'odd'];
+  data: string[] = ['one', 'two', 'three', 'four', 'five']; 
 
 }
--- src/app/dashboard/multa/multa.component.ts
+++ src/app/dashboard/multa/multa.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-multa',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatTableModule],
   templateUrl: './multa.component.html',
   styleUrl: './multa.component.css'
 })
 export class MultaComponent {
+  empleados: any;
+displayedColumns: string[] = ['$implicit', 'index', 'count', 'first', 'last', 'even', 'odd'];
+  data: string[] = ['one', 'two', 'three', 'four', 'five']; 
+
 
 }
